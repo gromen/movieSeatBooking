16ff731234d694b4fd03b55d8e10642ba7c3e6fd
Marcin Gromek <gromson101@gmail.com>
2021-05-18 13:32:47 +0200

    config: initial config

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+import './src/js/test';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentations</a>
+`
