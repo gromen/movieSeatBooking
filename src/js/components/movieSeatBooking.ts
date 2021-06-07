1d498ee305a86d1cf4b2cd2046023b14e9e22d05
Marcin Gromek <gromson101@gmail.com>
2021-06-07 14:31:12 +0200

    refactor: reorganize JS methods, HTML markup

--- src/js/components/movieSeatBooking.ts
+++ src/js/components/movieSeatBooking.ts
@@ -1,19 +1,31 @@
-export default class movieSeatBooking{
-  CSS_SEAT_SELECTED = 'c-seatBooking__seat--selected';
-  CSS_SEAT_OCCUPIED = 'c-seatBooking__seat--occupied';
+export default class movieSeatBooking {
+  CSS_SEAT_SELECTED = 'cinemaHall__seat--selected';
+  CSS_SEAT_OCCUPIED = 'cinemaHall__seat--occupied';
+  CSS_HIDDEN = 'd-none';
   ALL_SEATS_AMOUNT = 40;
 
-  container:HTMLElement | null;
-  seats:Array<HTMLElement>;
-  select:HTMLSelectElement | null;
-  textSummary:HTMLElement | null;
+  container: HTMLElement | null;
+  seats: Array<HTMLElement> = [];
+  select: HTMLSelectElement | null;
+  textSummary: HTMLElement | null;
+  buttonBuy: HTMLButtonElement | null;
 
   constructor(classContainer: string) {
     this.container = document.querySelector(classContainer);
+
+    if (!this.container) {
+      return;
+    }
+
     this.mountAllSeats();
-    this.seats = [...this.container!.querySelectorAll('.js-c-seatBooking__seat')] as Array<HTMLElement>;
-    this.select = document.querySelector('#selectMovie');
-    this.textSummary = <HTMLElement> document.querySelector('.js-c-seatBooking__summary');
+    this.seats = [
+      ...this.container!.querySelectorAll('.js-cinemaHall__seat'),
+    ] as Array<HTMLElement>;
+    this.select = document.querySelector('.js-cinemaHall__select');
+    this.textSummary = <HTMLElement>(
+      document.querySelector('.js-cinemaHall__summaryBooking')
+    );
+    this.buttonBuy = document.querySelector('.js-cinemaHall__btnBuy');
   }
 
   mount() {
@@ -23,18 +35,27 @@ export default class movieSeatBooking{
   }
 
   attachEvents() {
-    this.seats.forEach(seat => seat.addEventListener('click', () => {
-      if (!seat.classList.contains(this.CSS_SEAT_OCCUPIED)) {
-        this.clickHandler(seat);
-      }
-    }));
+    this.seats.forEach((seat) =>
+      seat.addEventListener('click', () => {
+        if (!seat.classList.contains(this.CSS_SEAT_OCCUPIED)) {
+          this.clickHandler(seat);
+        }
+      })
+    );
 
     this.select?.addEventListener('change', this.selectOnChangeHandler, false);
+    this.buttonBuy?.addEventListener('click', this.btnBuyClickHandler, false);
   }
 
   clickHandler = (seat) => {
-    seat.classList.toggle(this.CSS_SEAT_SELECTED, !seat.classList.contains(this.CSS_SEAT_SELECTED));
-
+    seat.classList.toggle(
+      this.CSS_SEAT_SELECTED,
+      !seat.classList.contains(this.CSS_SEAT_SELECTED)
+    );
+    this.buttonBuy?.classList.toggle(
+      this.CSS_HIDDEN,
+      this.allSelectedLength === 0
+    );
     this.updateTextSummary();
     this.selectedSeatsIndices();
   };
@@ -42,19 +63,40 @@ export default class movieSeatBooking{
   selectOnChangeHandler = () => {
     this.updateTextSummary();
     this.updateOptionSelected();
-    // this.saveMovieData(this.getSelectedSeats(), this.optionSelectedValue);
-  }
+  };
+
+  btnBuyClickHandler = () => {
+    let occupiedIndices: any = [];
+
+    if (this.selectedSeatIndices.length <= 1) return;
+
+    console.log(this.selectedSeatIndices);
+
+    this.seats.forEach((seat, index) => {
+      if (this.selectedSeatIndices.indexOf(index) > -1) {
+        seat.classList.remove(this.CSS_SEAT_SELECTED);
+        seat.classList.add(this.CSS_SEAT_OCCUPIED);
+        occupiedIndices.push(index);
+      }
+      localStorage.setItem(
+        'occupiedSeatIndices',
+        JSON.stringify(occupiedIndices)
+      );
+    });
+  };
 
   mountAllSeats() {
     for (let i = 0; i < this.ALL_SEATS_AMOUNT; i++) {
       const seat = document.createElement('div');
-      seat.setAttribute('class', 'c-seatBooking__seat js-c-seatBooking__seat');
+      seat.setAttribute('class', 'cinemaHall__seat js-cinemaHall__seat');
       this.container!.appendChild(seat);
     }
   }
 
   get allSelectedLength() {
-    return this.seats.filter(seat => seat.classList.contains(this.CSS_SEAT_SELECTED)).length;
+    return this.seats.filter((seat) =>
+      seat.classList.contains(this.CSS_SEAT_SELECTED)
+    ).length;
   }
 
   get optionSelectedValue() {
@@ -65,33 +107,57 @@ export default class movieSeatBooking{
     return this.select!.selectedIndex;
   }
 
-  getSelectedOption(index)  {
-    return this.select!.selectedIndex = index;
+  get selectedSeatIndices() {
+    return [...document.querySelectorAll(`.${this.CSS_SEAT_SELECTED}`)].map(
+      (seat) => this.seats.indexOf(seat as HTMLElement)
+    );
+  }
+
+  get occupiedSeatIndices() {
+    return [...document.querySelectorAll(`.${this.CSS_SEAT_OCCUPIED}`)].map(
+      (seat) => this.seats.indexOf(seat as HTMLElement)
+    );
+  }
+
+  getSelectedOption(index) {
+    return (this.select!.selectedIndex = index);
   }
 
   getSelectedSeats = () => this.allSelectedLength;
 
   selectedSeatsIndices() {
-    const selectedSeats =
-      document.querySelectorAll(`.${this.CSS_SEAT_SELECTED}, ${this.CSS_SEAT_OCCUPIED}`);
-    console.log(selectedSeats);
     this.getSelectedOption(localStorage.getItem('optionSelectedIndex'));
 
-    // @ts-ignore
-    const selectedSeatsIndices = [...selectedSeats].map(seat => this.seats.indexOf(seat));
-    console.log(selectedSeatsIndices);
-    localStorage.setItem('selectedSeatsIndices', JSON.stringify(selectedSeatsIndices));
-
+    localStorage.setItem(
+      'selectedSeatsIndices',
+      JSON.stringify(this.selectedSeatIndices)
+    );
   }
 
   retrieveSelectedSeatsAfterRefresh() {
-    const selectedSeatsIndices =
-      JSON.parse(<string>localStorage.getItem('selectedSeatsIndices'));
+    const selectedSeatsIndices = JSON.parse(
+      <string>localStorage.getItem('selectedSeatsIndices')
+    );
+    const occupiedSeatIndices = JSON.parse(
+      <string>localStorage.getItem('occupiedSeatIndices')
+    );
 
-    if (selectedSeatsIndices != null && selectedSeatsIndices.length > 0) {
+    if (
+      selectedSeatsIndices != null &&
+      selectedSeatsIndices.length > 0 &&
+      occupiedSeatIndices != null &&
+      occupiedSeatIndices.length === 0
+    ) {
+      console.log('assa');
       this.seats.forEach((seat, index) => {
         if (selectedSeatsIndices.indexOf(index) > -1) {
           seat.classList.add(this.CSS_SEAT_SELECTED);
+        }
+      });
+    } else {
+      this.seats.forEach((seat, index) => {
+        if (selectedSeatsIndices.indexOf(index) > -1) {
+          seat.classList.add(this.CSS_SEAT_OCCUPIED);
         }
       });
     }
@@ -102,9 +168,9 @@ export default class movieSeatBooking{
   }
 
   updateTextSummary = () => {
-    this.textSummary!.innerHTML =
-      `You have selected ${this.getSelectedSeats()} for a price ${this.totalPrice()}$`
-  }
+    this.textSummary!.innerHTML = `You have selected ${this.getSelectedSeats()} for a price ${this.totalPrice()}$`;
+  };
 
-  totalPrice = () => this.getSelectedSeats() * parseInt(this.optionSelectedValue, 10)
+  totalPrice = () =>
+    this.getSelectedSeats() * parseInt(this.optionSelectedValue, 10);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import './scss/style.scss';
 
-import './js/test';
 import MovieSeatBooking from './js/components/movieSeatBooking';
 
-document.querySelector('.js-c-seatBooking')!.innerHTML = `
-  <h1>Movie Seat booking</h1>
-  <div class="c-seatBooking__seats js-c-seatBooking__seats"></div>
-  <div class="js-c-seatBooking__summary">You have selected {{SELECTED_SEATS}} for a price {{SELECTED_SEATS}}$</div>
-`
-new MovieSeatBooking('.js-c-seatBooking__seats').mount();
+const movieSeatBooking = new MovieSeatBooking('.js-cinemaHall__seats');
+movieSeatBooking.mount();
